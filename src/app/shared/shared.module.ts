a21e199d6de43fbb1e97a381924f8d603d67f939
Joe Barrett <joebarrett401@gmail.com>
2018-06-02 10:08:34 +0100

    Add semantic ui

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
+import {SuiModule} from "ng2-semantic-ui";
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    SuiModule
   ],
   declarations: [
   ],
